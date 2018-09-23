9a5da8cbfd7c330b3d3567a97b60524d4a22fb8e
Alek Merani <alekmerani@gmail.com>
2018-09-23 18:42:06 -0500

    plugins: Add rc file with plugins

---
+++ src/plugins.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+function get() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.resolve(process.cwd(), ".bettercommitrc"), { encoding: 'utf-8'}, (err, data) => {
+            if(err) reject(err);
+            const plugins = JSON.parse(data).plugins.map(plugin => {
+                const local = path.resolve(process.cwd(), plugin);
+                if(fs.existsSync(`${local}.js`) || fs.existsSync(local)) {
+                    return local.toString();
+                }
+                return plugin;
+            });
+            resolve(plugins);
+        })
+    })
+}
+
+module.exports = {
+    get
+};
