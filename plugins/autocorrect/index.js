9fca55596a13386e10430dc4380a0e61c961488e
Alek Merani <alekmerani@gmail.com>
2018-10-02 22:23:41 -0500

    Add autocorrect plugin

---
+++ plugins/autocorrect/index.js
@@ -0,0 +1,34 @@
+var dictionary = require('dictionary-en-us');
+var nspell = require('nspell');
+
+async function autocorrect({ commit, branch }) {
+    let { message } = commit;
+
+    const nspellPromise = new Promise((resolve, reject) => {
+        dictionary(function(err, dict) {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            const spell = nspell(dict);
+            resolve(spell);
+        })
+    })
+
+    const speller = await nspellPromise;
+    if(speller) {
+        const corrected = message.split(" ")
+            .map(w => {
+                if(!speller.correct(w)) {
+                    return speller.suggest(w)[0];
+                }
+                return w;
+            })
+            .join(" ");
+        commit.message = corrected;
+        return commit;
+    }
+    return commit;
+}
+
+module.exports = autocorrect;
