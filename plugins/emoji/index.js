37e3f28404f6d97ef4031874580fc4c09b2e3495
Alek Merani <alekmerani@gmail.com>
2018-10-05 18:10:15 -0500

    mr: Random emoji 🐘

--- plugins/emoji/index.js
+++ plugins/emoji/index.js
@@ -1,6 +1,12 @@
+const randomEmoji = require("random-emoji");
+
 function emojify({ commit, /*branch*/ }, options) {
     let { message } = commit;
-    if(options) {
+    if(options.toLowerCase() === "random") {
+        const emoji = randomEmoji.random({count:1});
+        commit.message = `${message} ${emoji[0].character}`
+    }
+    else if(options) {
         commit.message = `${message} ${options}`
     }
     return commit;
